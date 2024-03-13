9ca99c19d4e86cf03d936af05f00aab2728f5c40
Eduard Gumen <hydrospirt@mail.ru>
2024-03-14 00:15:55 +0300

    feature: Add Account model

--- hrspace/accounts/models.py
+++ hrspace/accounts/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Account(models.Model):
+    """Модель Аккаунта, расширяющая модель
+    Пользователя с помощью подключения OneToOne."""
+
+    class Role(models.TextChoices):
+        """Список доступных значений для поля 'роль'."""
+        HEADHUNTER = 'HH', 'Рекрутер',
+        HH_AGENCY = 'AH', 'Кадровое Агенство',
+        COMPANY = 'СO', 'Компания'
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE,
+                                verbose_name='Пользователь')
+    patronymic = models.CharField(
+        max_length=50,
+        blank=False,
+        null=False,
+        verbose_name='Отчество'
+    )
+    phone = models.CharField(
+        max_length=17,
+        blank=False,
+        null=False,
+    )
+    role = models.CharField(
+        max_length=18,
+        blank=False,
+        choices=Role.choices,
+        default=Role.COMPANY,
+        verbose_name='Роль',
+    )
+
+    class Meta:
+        verbose_name = 'Аккаунт'
+        verbose_name_plural = 'Аккаунты'
+
+    def __str__(self) -> str:
+        return f'Аккаунт {self.user.first_name} {self.patronymic}'
+
+
+class Company(models.Model):
+    ...
