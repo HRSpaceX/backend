4c71aa0be3d7bddfc4f9ae5f1d025ac2d11f2998
JuliSem <julia.sem295@yandex.ru>
2024-03-16 23:25:53 +0300

    Редактирование модели Заявка + добавление новых + редактирование файла с константами

--- hrspace/bid/admin.py
+++ hrspace/bid/admin.py
@@ -1,3 +1,11 @@
-# from django.contrib import admin
+from django.contrib import admin
+from .models import (BenefitsPackage, City, HrResponsibility, Order,
+                     Profession, TypeEmployment, Skill)
 
-# Register your models here.
+admin.site.register(BenefitsPackage)
+admin.site.register(City)
+admin.site.register(HrResponsibility)
+admin.site.register(Order)
+admin.site.register(Profession)
+admin.site.register(TypeEmployment)
+admin.site.register(Skill)
