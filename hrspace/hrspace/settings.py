83fa39f7a505ff3aa50725d70e3ab27df2e1d727
Eduard Gumen <hydrospirt@mail.ru>
2024-03-16 16:35:05 +0300

    feature: Add new app accounts

--- hrspace/hrspace/settings.py
+++ hrspace/hrspace/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bid.apps.BidConfig"
+    "bid.apps.BidConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
