f25bf4e4973e0aa61256853111664a438b83fc1f
Eduard Gumen <hydrospirt@mail.ru>
2024-03-16 18:46:12 +0300

    feature: Add AccountUserAdmin in User admin panel

--- hrspace/accounts/admin.py
+++ hrspace/accounts/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+from accounts.models import Account
 
-# Register your models here.
+User = get_user_model()
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'phone',
+        'role',
+    )
+
+
+class AccountInline(admin.StackedInline):
+    model = Account
+    can_delete = False
+    verbose_name_plural = 'Аккаунты'
+
+
+class AccountUserAdmin(UserAdmin):
+    inlines = (AccountInline, )
+    form = UserChangeForm
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+class HeadHunterAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+    )
+
+
+class HeadHunterAgencyAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, AccountUserAdmin)
